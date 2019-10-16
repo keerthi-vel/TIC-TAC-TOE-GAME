5e1b06439ed0d78251675bab13a584cda614061c
keerthi <velaga88@gmail.com>
2019-10-16 16:57:22 -0400

    game with all logic and display

--- js/game.js
+++ js/game.js
@@ -1,7 +1,16 @@
 const moves = [];
 let turn = 0;
+var p1 =0;
+var p2 =0;
+//var winner = false;
+
+const play1 = document.querySelector('p.player1');
+console.log(play1);
+const play2 = document.querySelector('p.player2');
+console.log(play2);
+
+const c= document.querySelector('.board'); 
 const boardValue = document.querySelectorAll('.cell');
-// loop through boardValue, each time it will give us a DOM element, console.log each element
 for (let i = 0; i < boardValue.length; i++) {
     const cellButton = document.createElement('button');
     cellButton.innerText = '';
@@ -10,16 +19,20 @@ for (let i = 0; i < boardValue.length; i++) {
         console.log(event);
         console.log(this.id);
         console.log('the id clicked on', this.getAttribute('id'));
-        if (turn % 2 === 0)
+        if (turn % 2 === 0 && turn < 9)
         {
             if (moves[this.id] !== 'O' && moves[this.id]!== 'X') 
             {
                 moves[this.id] = 'X';
                 this.innerText = 'X';
-                console.log('player1 is playing');
+                console.log('Player1 is playing');
                 if(moves[0] === 'X' && moves[1] === 'X' && moves[2] === 'X'|| moves[3] === 'X' && moves[4] === 'X' && moves[5] === 'X'|| moves[6] === 'X' && moves[7] === 'X' && moves[8] === 'X'|| moves[0] === 'X' && moves[3] === 'X' && moves[6] === 'X'|| moves[1] === 'X' && moves[4] === 'X' && moves[7] === 'X'|| moves[2] === 'X' && moves[5] === 'X' && moves[8] === 'X'|| moves[0] === 'X' && moves[4] === 'X' && moves[8] === 'X'|| moves[2] === 'X' && moves[4] === 'X' && moves[6] === 'X')
-                {
-                console.log('player1 wins');
+                {    p1 = p1+1;
+                    alert(" Player 1 wins");
+                    console.log('Player 1 wins',p1);  
+                    play1.innerText = "PLAYER 1 WON";
+                    c.style.pointerEvents = 'none';
+                    //winner = true;
                 }
             }
         }
@@ -29,19 +42,36 @@ for (let i = 0; i < boardValue.length; i++) {
             {
                     moves[this.id] = 'O';
                     this.innerText = 'O';
-                    console.log('player2 is playing');
+                    console.log('Player2 is playing');
                     if(moves[0] === 'O' && moves[1] === 'O' && moves[2] === '0'|| moves[3] === 'O' && moves[4] === 'O' && moves[5] === 'O'|| moves[6] === 'O' && moves[7] === 'O' && moves[8] === 'O'|| moves[0] === 'O' && moves[3] === 'O' && moves[6] === 'O'|| moves[1] === 'O' && moves[4] === 'O' && moves[7] === 'O'|| moves[2] === 'O' && moves[5] === 'O' && moves[8] === 'O'|| moves[0] === 'O' && moves[4] === 'O' && moves[8] === 'O'|| moves[2] === 'O' && moves[4] === 'O' && moves[6] === 'O')
-                {
-                console.log('player2 wins');
+                {   p2 = p2+1;
+                    alert(" Player2 wins");
+                    console.log('player2 wins', p2);
+                    play2.innerText = "PLAYER 2 WON";
+                    c.style.pointerEvents = 'none';
+                    //winner = true;
                 }
             }
         }    
         console.log(moves, 'moves');
         turn++;
-    });
+        const win = document.getElementById('score');
+    win.addEventListener('click',function(){
+        if(p1>p2 && win === true)
+         {win.innerText="Player1 won game";
+         }
+        else if(turn===9 && p1===0 && p2===0){
+            win.innerText = "draw";
+         }
+         else{
+            win.innerText = "Player2 won";
+            }
+        });
+        
+    },{once : true});
 }
-
-
+console.log('game end');
 function restartf() {
     window.location.reload()
 }
+//const win= document.getElementById('score').addEventListener('click',function(){alert("player2 wins!");});
